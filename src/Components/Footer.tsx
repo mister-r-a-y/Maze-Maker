9c14846b64cf9d4beb3e654dcfd370ed649dcfd7
mister-r-a-y <mblack0619@gmail.com>
2023-10-02 17:32:17 -0400

    Create alt description for github logo

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,39 +1,43 @@
-import { 
-  Box, 
-  Container, 
-  Stack, 
-  Text,
-  Image
-} 
-  from '@chakra-ui/react'
-
-
+import { Box, Container, Stack, Text, Image } from "@chakra-ui/react";
 
 export default function Footer() {
   return (
     <Box>
       <Container
         as={Stack}
-        maxW={'6x1'}
+        maxW={"6x1"}
         py={4}
-        direction={{ base: 'column', md: 'row'}}
+        direction={{ base: "column", md: "row" }}
         spacing={4}
-        justify={{ base: 'center', md: 'space-between' }}
-        align={{ base: 'center', md: 'center' }}>
-        <Stack direction={'row'} spacing={6} align='center' justify='center'>
-          <Box as="a" _hover={{color: 'green'}} href={'https://github.com/mister-r-a-y/maze-maker'}>
+        justify={{ base: "center", md: "space-between" }}
+        align={{ base: "center", md: "center" }}
+      >
+        <Stack
+          direction={"row"}
+          spacing={6}
+          align="center"
+          justify="center">
+          <Box
+            as="a"
+            _hover={{ color: "green" }}
+            href={"https://github.com/mister-r-a-y/maze-maker"}
+          >
             <Image
-              boxSize='18px'
-              objectFit='cover'
-              src='/github.svg'
-             />
+              boxSize="18px"
+              objectFit="cover"
+              src="/github.svg"
+              alt='Github Repo' />
           </Box>
-          <Box as="a" _hover={{color: 'green'}} href={'mailto: mblack0619@gmail.com'}>
+          <Box
+            as="a"
+            _hover={{ color: "green" }}
+            href={"mailto: mblack0619@gmail.com"}
+          >
             Contact
           </Box>
         </Stack>
         <Text>made by Michael Blackmon</Text>
       </Container>
     </Box>
-  )
+  );
 }
